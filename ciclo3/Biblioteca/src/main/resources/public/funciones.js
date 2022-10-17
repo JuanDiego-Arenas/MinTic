fefc10410e9273828c47bc8fd4bef1cf09c008f7
JuanDiego-Arenas <Arenas250421@gmail.com>
2022-10-16 23:10:57 -0500

    Fin de ciclo

--- ciclo3/Biblioteca/src/main/resources/public/funciones.js
+++ ciclo3/Biblioteca/src/main/resources/public/funciones.js
@@ -19,8 +19,8 @@ function mostrarTabla(filas){
         miTabla += "<td>" + filas[i].autor + "</td>";
         miTabla += "<td>" + filas[i].editorial + "</td>";
         miTabla += "<td>" + filas[i].no_page + "</td>";
-        miTabla+="<td><button onclick='cargarDatos("+filas[i].isbn+")'>Editar</button>"
-        miTabla+="<td><button onclick='eliminarLibro("+filas[i].isbn+")'>Eliminar</button>"
+        miTabla+="<td><button onclick='cargarDatos(\""+filas[i].isbn+"\")'>Editar</button>"
+        miTabla+="<td><button onclick='eliminarLibro(\""+filas[i].isbn+"\")'>Eliminar</button>"
         miTabla += "</tr>"
     }
     miTabla += "</table>";
@@ -36,10 +36,13 @@ function consultarLibro(){
         datatype: "JSON",
         success:function(respuesta){
             console.log(respuesta);
-            $('#tabla').empty();
-            texto=respuesta.isbn+" -- "+respuesta.titulo+" -- "+respuesta.autor + "\n";
-            $('#tabla').empty();
-            $('#libro').append(texto);
+            if(respuesta != null){
+                $('#tabla').empty();
+                texto=respuesta.isbn+" -- "+respuesta.titulo+" -- "+respuesta.autor + "\n";
+                $('#libro').append(texto);
+            }else{
+                alert("El libro no se encontró")
+            }
         }
     });
 }
@@ -78,5 +81,69 @@ function guardarLibro(){
             alert(respuesta)
         }
     });
+}
 
+function cargarDatos(codigo){
+    alert(codigo);
+    $.ajax({
+        url: "http://localhost:8080/BuscarLibro/" + codigo,
+        type: "GET",
+        datatype: "JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            $('#isbn_insert').val(respuesta.isbn);
+            $('#isbn_insert').attr('disabled','disabled');
+            $('#titulo').val(respuesta.titulo);
+            $('#autor').val(respuesta.autor);
+            $('#editorial').val(respuesta.editorial);
+            $('#paginas').val(respuesta.no_page);
+        }
+    });
+}
+
+function actualizarLibro(){
+    let datos = {
+        isbn:$('#isbn_insert').val(),
+        titulo:$('#titulo').val(),
+        autor:$('#autor').val(),
+        editorial:$('#editorial').val(),
+        no_page:$('#paginas').val()
+    }
+
+    let datosJson = JSON.stringify(datos);
+
+    $.ajax({
+        url: "http://localhost:8080/ActualizarLibro",
+        type: "PUT",
+        data: datosJson,
+        contentType: "application/JSON",
+        datatype: "JSON",
+        success: function(respuesta){
+            console.log(respuesta);
+            alert(respuesta);
+        }
+    });
+
+    $('#isbn_insert').val("");
+    $('#isbn_insert').removeAttr('disabled');
+    $('#titulo').val("");
+    $('#autor').val("");
+    $('#editorial').val("");
+    $('#paginas').val("");
+    listarLibros();
+}
+
+function eliminarLibro(codigo){
+    var opcion = confirm("Estas seguro de eliminar esste libro " + codigo);
+    if(opcion === true){
+        $.ajax({
+            url: "http://localhost:8080/EliminarLibro/" + codigo,
+            type: "DELETE",
+            datatype: "JSON",
+            success: function(respuesta){
+                listarLibros();
+                alert(respuesta);
+            }
+        });
+    }
 }
